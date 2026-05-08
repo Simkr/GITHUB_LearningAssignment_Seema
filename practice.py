3956c8977ad23f6d3f79c934ff3bb7e5a60de6af
Simkr <seemakr1122@gmail.com>
2026-05-08 13:13:51 +0530

    New program added

--- practice.py
+++ practice.py
@@ -1,1 +1,10 @@
-print("New feature added")
+########################################
+def remove_chars(word, n):
+    print('Original string:', word)
+    # Extract string from index n to the end
+    res = word[n:]
+    return res
+
+print("Removing characters from a string")
+print(remove_chars("pynative", 4))
+print(remove_chars("pynative", 2))
